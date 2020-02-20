bf6c721992861ae783f58d61921741ba9e612a05
Fraleym7700 <54638784+Fraleym7700@users.noreply.github.com>
2020-02-20 10:45:05 -0500

    Add files via upload

--- HoursTraveledProgram.py
+++ HoursTraveledProgram.py
@@ -1,11 +1,14 @@
-mylist = [3,6,9]
-speed = 65
+mylist = []
+amountOfHours = int(input("Enter the amount of hours:"))
+for x in range(amountOfHours):
+    mylist.append(int(input()))
+speed = int(input("Enter the speed you car is going: "))
 total = 0
 print("Hours \t DistanceTraveled")
 for index, num in enumerate(mylist):
     result = num * speed
     mylist[index] = result
-    total += result
+    total = total + result
     print(num, "\t", result)
     
 print("Total\t",total)
